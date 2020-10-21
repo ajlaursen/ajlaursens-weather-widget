c7183a88b4ea2f3959b1e08f4f0a26069ab58417
Alex Laursen <ajlaursen@aol.com>
2020-10-20 21:34:21 -0400

    started on JS file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var cityName = "";
-var weatherAppUrl = "api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=ca202c6ae2ceb6ff80e75b7ff67eaf7d";
-
+// var weatherAppUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=ca202c6ae2ceb6ff80e75b7ff67eaf7d";
+var userEntry = JSON.parse(localStorage.getItem("userEntry")) || [];
 // gather user input and call api
 // store data to an array
 // save array to local storage
@@ -9,6 +9,23 @@ var weatherAppUrl = "api.openweathermap.org/data/2.5/forecast?q="+cityName+"&app
 // populate DOM with information after button clicked
 
 
-$("#search-button").click(function(input){
+$("#search-button").click(function(){
+    userEntry.splice(0,0, $("#city-input").val().trim());
+    localStorage.setItem("userEntry", JSON.stringify(userEntry));
+    apiCall(userEntry[0])
+});
 
-})
+function apiCall(userSelection){
+    cityName = userSelection
+    var weatherAppUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=ca202c6ae2ceb6ff80e75b7ff67eaf7d";
+    $.ajax({
+        url: weatherAppUrl,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
+}
+
+function populateForecast (){
+    
+}
