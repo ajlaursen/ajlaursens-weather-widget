578b90a01acb29754ffdab8b616d711dc4fe9dca
Alex Laursen <ajlaursen@aol.com>
2020-10-20 20:03:10 -0400

    finished HTML layout

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var cityName = "";
+var weatherAppUrl = "api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=ca202c6ae2ceb6ff80e75b7ff67eaf7d";
+
+// gather user input and call api
+// store data to an array
+// save array to local storage
+// populate DOM with information from local storage on load
+// populate DOM with information after button clicked
+
+
+$("#search-button").click(function(input){
+
+})
